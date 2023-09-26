d58880d934f1f601d449042d823b1f116c729345
Lorena Grigoraki <lorenagrigoraki98@gmail.com>
2023-09-26 19:48:22 +0300

    the test for gemboard.js

---
+++ gameBoard.test.js
@@ -0,0 +1,79 @@
+const gameBoard = require('./gameBoard')
+
+test('Test if the gameboard class is returning the board', () => {
+    const board = new gameBoard()
+    expect(board).toEqual(
+        {
+            "cleanBoard": [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+        })
+})
+test('Test if the shipship method is returning a ship', () => {
+    const board = new gameBoard()
+    expect(board.shipship(3)).toEqual(
+        {
+            "shipLength": 3,
+            "demageHit": 0,
+            "sunk": false
+        })
+})
+
+test('should place a ship of length 3 horizontally at coordinates (0, 5)', () => {
+    const board = new gameBoard();
+    board.placeTheShip(0, 5, 3, 'X');
+
+    expect(board).toEqual(
+        {
+            "cleanBoard": [
+                [0, 0, 0, 0, 0, 'S', 'S', 'S', 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+        });
+});
+test('should place a ship of length 3 vertically at coordinates (3,2)', () => {
+    const board = new gameBoard();
+    board.placeTheShip(3, 2, 3, 'Y');
+
+    expect(board.cleanBoard).toEqual(
+               [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 'S', 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 'S', 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 'S', 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]);
+});
+test('should place a ship of length 3 horizontally to the end of the row', () => {
+    const board = new gameBoard();
+    board.placeTheShip(1, 9, 3, 'X');
+
+    expect(board.cleanBoard).toEqual(
+               [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 'S', 'S', 'S'],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]);
+});
