36131fc07a1dbda3bf7e3cbbbb17542e27e9cb5c
Lorena Grigoraki <lorenagrigoraki98@gmail.com>
2023-10-04 05:06:29 +0300

    modified the shipship method to add the new ship to the this.ships

--- gameBoard.js
+++ gameBoard.js
@@ -1,39 +1,42 @@
 const ship = require('./ship')
-class gameBoard{
-    constructor(){
-       this.cleanBoard = this.makeTheBoard()
+class gameBoard {
+  constructor() {
+    this.cleanBoard = this.makeTheBoard()
+    this.ships = []
+  }
+  makeTheBoard() {
+    let b = []
+    for (let i = 0; i < 10; i++) {
+      let temp = []
+      for (let j = 0; j < 10; j++) {
+        temp.push(0)
+      }
+      b.push(temp)
     }
-    makeTheBoard(){
-        let b = []
-        // let cleanRows = new Array(10)
-        for(let i = 0; i<10;i++){
-            let temp = []
-            for(let j = 0; j < 10; j++){
-                temp.push(0)
-            }
-            b.push(temp)    
-        }
-        return b
+    return b
+  }
+  shipship(len) {
+    let newShip = new ship(len)
+    let thisShips = this.ships
+    thisShips.push(newShip)
+    return newShip
+  }
+  placeTheShip(yCoordinate, xCoordinate, shipLength, orientation) {
+    if (orientation === 'X') {
+      let helpCoordinate = (xCoordinate + shipLength >= 9) ? 10 - shipLength : xCoordinate
+      let helpIterator = (xCoordinate + shipLength >= 9) ? 10 : xCoordinate + shipLength
+      for (let i = helpCoordinate; i < helpIterator; i++) {
+        this.cleanBoard[yCoordinate][i] = 'S';
+      }
+    } else if (orientation === 'Y') {
+      let helpCoordinate = (yCoordinate + shipLength >= 9) ? 10 - shipLength : yCoordinate
+      let helpIterator = (yCoordinate + shipLength >= 9) ? 10 : yCoordinate + shipLength
+      for (let i = helpCoordinate; i < helpIterator; i++) {
+        this.cleanBoard[i][xCoordinate] = 'S';
+      }
     }
-    shipship(len){
-        return new ship(len)
-    }
-    placeTheShip(yCoordinate, xCoordinate, shipLength, orientation) {       
-        if (orientation === 'X') {
-            let helpCoordinate = (xCoordinate + shipLength >= 9) ? 10 - shipLength : xCoordinate
-            let helpIterator =(xCoordinate + shipLength >= 9) ? 10 : xCoordinate + shipLength
-            for (let i = helpCoordinate; i < helpIterator; i++) {
-              this.cleanBoard[yCoordinate][i] = 'S';
-            }
-          } else if (orientation === 'Y') {
-            let helpCoordinate = (yCoordinate + shipLength >= 9) ? 10 - shipLength : yCoordinate
-            let helpIterator =(yCoordinate + shipLength >= 9) ? 10 : yCoordinate + shipLength
-            for (let i = helpCoordinate; i < helpIterator; i++) {
-              this.cleanBoard[i][xCoordinate] = 'S';
-            }
-          }
-          return this.cleanBoard
-        }
+    return this.cleanBoard
+  }
 }
 
 
