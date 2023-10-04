35b0cbbe1e6b3c136674b54214c04af6ff42322e
Lorena Grigoraki <lorenagrigoraki98@gmail.com>
2023-10-04 05:48:30 +0300

    add a new method - receiveAttack

--- gameBoard.js
+++ gameBoard.js
@@ -22,22 +22,36 @@ class gameBoard {
     return newShip
   }
   placeTheShip(yCoordinate, xCoordinate, shipLength, orientation) {
+    let newShip = new ship(shipLength)
     if (orientation === 'X') {
       let helpCoordinate = (xCoordinate + shipLength >= 9) ? 10 - shipLength : xCoordinate
       let helpIterator = (xCoordinate + shipLength >= 9) ? 10 : xCoordinate + shipLength
       for (let i = helpCoordinate; i < helpIterator; i++) {
-        this.cleanBoard[yCoordinate][i] = 'S';
+        this.cleanBoard[yCoordinate][i] = newShip;
       }
     } else if (orientation === 'Y') {
       let helpCoordinate = (yCoordinate + shipLength >= 9) ? 10 - shipLength : yCoordinate
       let helpIterator = (yCoordinate + shipLength >= 9) ? 10 : yCoordinate + shipLength
       for (let i = helpCoordinate; i < helpIterator; i++) {
-        this.cleanBoard[i][xCoordinate] = 'S';
+        this.cleanBoard[i][xCoordinate] = newShip;
       }
     }
+    this.shipship(shipLength)
     return this.cleanBoard
   }
+  receiveAttack(yCoordinate, xCoordinate){
+    const board = this.cleanBoard
+    let allShips = this.ships
+    if(board[yCoordinate][xCoordinate] !== 0){
+      const hitShip = allShips[allShips.indexOf(board[yCoordinate][xCoordinate])]
+      // return 'The ship is hit!'
+      return board[yCoordinate][xCoordinate]
+    }else{
+      return 'You mised your shot!'
+    }
+  }
 }
+
 
 
 module.exports = gameBoard
