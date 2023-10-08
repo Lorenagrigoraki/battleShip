90e5befb9a6443cc7600e6ae5c2489b3ddc1a015
Lorena Grigoraki <lorenagrigoraki98@gmail.com>
2023-10-08 11:56:43 +0300

    add test cases to check if the receiveAtack method to ckecks if all ships are sunk

--- gameBoard.test.js
+++ gameBoard.test.js
@@ -16,167 +16,7 @@ test('Test if the gameboard class is returning the board', () => {
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
     )
 })
-//Закоментила этот тест потому что я создала новый класс ship с айди для каждого 
-//корабля
-
-// test('Test if the shipship method is returning a ship', () => {
-//     const board = new gameBoard()
-//     let newShip = new ship(3)
-//     expect(board.shipship(newShip)).toEqual(
-//         {
-//             "shipLength": 3,
-//             "demageHit": 0,
-//             "sunk": false
-//         })
-// })
-
-// test('should place a ship of length 3 horizontally at coordinates (0, 5)', () => {
-//     const board = new gameBoard();
-//     board.placeTheShip(0, 5, 3, 'X');
-
-//     expect(board.cleanBoard).toEqual(
-//         [[0, 0, 0, 0, 0, 'S', 'S', 'S', 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-//         ])
-// });
-
-// test('should place a ship of length 3 vertically at coordinates (3,2)', () => {
-//     const board = new gameBoard();
-//     board.placeTheShip(3, 2, 3, 'Y');
-
-//     expect(board.cleanBoard).toEqual(
-//         [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 'S', 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 'S', 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 'S', 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]);
-// });
-// test('should place a ship of length 3 horizontally to the end of the row', () => {
-//     const board = new gameBoard();
-//     board.placeTheShip(1, 9, 3, 'X');
-
-//     expect(board.cleanBoard).toEqual(
-//         [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 'S', 'S', 'S'],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]);
-// });
-// test('should place a ship of length 2 vertically to the end of the column', () => {
-//     const board = new gameBoard();
-//     board.placeTheShip(9, 3, 2, 'Y');
-
-//     expect(board.cleanBoard).toEqual(
-//         [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 'S', 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 'S', 0, 0, 0, 0, 0, 0]]);
-// });
-// test('should place 2 different ships', () => {
-//     const board = new gameBoard();
-//     board.placeTheShip(9, 3, 2, 'Y');
-//     board.placeTheShip(2, 3, 4, 'X');
-
-//     expect(board.cleanBoard).toEqual(
-//         [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 'S', 'S', 'S', 'S', 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 'S', 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 'S', 0, 0, 0, 0, 0, 0]]);
-// });
-// test('should place a ship that goes beyond the gameboard X orientation', () => {
-//     const board = new gameBoard();
-//     board.placeTheShip(2, 7, 4, 'X');
-
-//     expect(board.cleanBoard).toEqual(
-//         [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 'S', 'S', 'S', 'S'],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]);
-// });
-// test('should place a ship that goes beyond the gameboard Y orientation', () => {
-//     const board = new gameBoard();
-//     board.placeTheShip(8, 1, 4, 'Y');
-
-//     expect(board.cleanBoard).toEqual(
-//         [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 'S', 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 'S', 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 'S', 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 'S', 0, 0, 0, 0, 0, 0, 0, 0]]);
-// });
-
-//Закоментила этот тест потому, что есть новый класс ship с айди
-//для каждого нового корабля
-// test('should add a ship to the ships array', () => {
-//     const board = new gameBoard();
-//     let newShip = new ship(4)
-//     board.shipship(newShip);
-
-//     expect(board.ships).toEqual([{ shipLength: 4, demageHit: 0, sunk: false }])
-// })
-
-// test('should place a ship and add a ship to this.ships', () => {
-//     const board = new gameBoard();
-//     board.placeTheShip(8, 1, 4, 'Y');
-
-//     expect(board).toEqual({
-
-//         'cleanBoard': [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 'S', 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 'S', 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 'S', 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 'S', 0, 0, 0, 0, 0, 0, 0, 0]],
-//         ships: [{ shipLength: 4, demageHit: 0, sunk: false }]
-//     });
-// });
-
-//Poke ne Rabotaet!
+//Rabotaet!
 test('should place a ship object to the board and add a ship to this.ships', () => {
     const board = new gameBoard();
     board.placeTheShip(8, 1, 4, 'Y');
@@ -207,11 +47,11 @@ test('should determines if this ship is hit', () => {
     const board = new gameBoard();
     board.placeTheShip(8, 1, 4, 'Y')
     board.placeTheShip(3, 2, 2, 'X')
-
-    expect(board.receiveAttack(3,3)).toEqual([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    board.receiveAttack(3,3)
+    expect(board.receiveAttack(3,2)).toEqual([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, { shipLength: 2, id: 1, demageHit: 1, sunk: false }, { shipLength: 2, id: 1, demageHit: 1, sunk: false }, 0, 0, 0, 0, 0, 0],
+        [0, 0, { shipLength: 2, id: 1, demageHit: 2, sunk: true }, { shipLength: 2, id: 1, demageHit: 2, sunk: true }, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, { shipLength: 4, id: 0, demageHit: 0, sunk: false }, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -240,4 +80,40 @@ test('should checks if its possible to place the ship in Y direction', () => {
 
     expect(board.placeTheShip(1, 5, 3, 'Y')).toEqual('You can not place the ship here!');
 });
+test('should determines if this ship is hit', () => {
+    const board = new gameBoard();
+    board.placeTheShip(8, 1, 4, 'Y')
+    board.placeTheShip(3, 2, 2, 'X')
+    board.placeTheShip(4, 0, 3, 'X')
 
+    board.receiveAttack(3,3)
+    board.receiveAttack(3,2)
+    board.receiveAttack(6,1)
+    board.receiveAttack(7,1)
+    board.receiveAttack(8,1)
+
+    expect(board.receiveAttack(9,1)).toEqual([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, { shipLength: 2, id: 1, demageHit: 2, sunk: true }, { shipLength: 2, id: 1, demageHit: 2, sunk: true }, 0, 0, 0, 0, 0, 0],
+        [{ shipLength: 3, id: 2, demageHit: 0, sunk: false }, { shipLength: 3, id: 2, demageHit: 0, sunk: false }, { shipLength: 3, id: 2, demageHit: 0, sunk: false }, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, { shipLength: 4, id: 0, demageHit: 4, sunk: true }, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, { shipLength: 4, id: 0, demageHit: 4, sunk: true }, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, { shipLength: 4, id: 0, demageHit: 4, sunk: true }, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, { shipLength: 4, id: 0, demageHit: 4, sunk: true }, 0, 0, 0, 0, 0, 0, 0, 0]]);
+})
+test('should determines if you missed a shot', () => {
+    const board = new gameBoard();
+    board.placeTheShip(8, 1, 4, 'Y')
+    board.placeTheShip(3, 2, 2, 'X')
+    board.placeTheShip(4, 0, 3, 'X')
+
+    board.receiveAttack(3,3)
+    board.receiveAttack(3,2)
+    board.receiveAttack(6,1)
+    board.receiveAttack(7,1)
+    board.receiveAttack(8,1)
+
+    expect(board.receiveAttack(0,0)).toEqual('You miss a shot!');
+})
